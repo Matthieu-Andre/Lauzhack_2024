1fd6bd4c9c647ff2e1e3252400f27aac4927ec9a
maschull106 <matthias.schuller@epfl.ch>
2024-12-01 00:22:11 +0100

    updated server

--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 import cv2
 import base64
 import numpy as np
-from db import DataBase
+from db import *
 
 app = FastAPI(title="aaaa")
 
@@ -13,19 +13,24 @@ class Image(BaseModel):
 
 
 @app.post("/{user_id}/image}")
-def hello(user_id: str, image: Image):
+def image(user_id: str, image: Image):
     decoded_bytes = base64.b64decode(image.frame_data)
     # Convert bytes to a numpy array
     np_array = np.frombuffer(decoded_bytes, np.uint8)
     # Decode the numpy array back to an image
     decoded_image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
-    server.db.store_image(decoded_image, f"{user_id}_test.jpg")
+    server.new_clothing_from_image(decoded_image, f"{user_id}_test.jpg")
     return "image saved"
 
 
 class Server:
     def __init__(self):
         self.db = DataBase()
+    
+    def new_clothing_from_image(self, user_id: str, image: cv2.typing.MatLike) -> None:
+        item = Clothing()
+        self.db.store_image(image, item.image_path)
+        self.db.add_clothing(user_id, item)
 
 
 server = Server()
