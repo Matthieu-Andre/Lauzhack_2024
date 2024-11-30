6e67b6006104976072131a3319ec1b72ec603fe3
maschull106 <matthias.schuller@epfl.ch>
2024-11-30 22:38:41 +0100

    advancement on db

--- server/db.py
+++ server/db.py
@@ -1,5 +1,70 @@
+from __future__ import annotations
 import os
 import cv2
+import sqlite3
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import json
+from clothing import *
+
+
+# Define the base class for all models
+Base = declarative_base()
+
+
+class SQLUser(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True)  # user_id as primary key
+    clothes = Column(String)  # Store serialized list as string
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._init_clothes()
+    
+    def _init_clothes(self) -> None:
+        self._set_clothes([])
+        
+    def _set_clothes(self, clothes: list[int]) -> None:
+        self.clothes = json.dumps(clothes)  # Convert list to JSON string
+
+    def get_clothes(self) -> list[int]:
+        return json.loads(self.clothes) if self.clothes else []
+    
+    def clothes_append(self, item: Clothing) -> None:
+        clothes = self.get_clothes()
+        clothes.append(item.id)
+        self._set_clothes(clothes)
+    
+    
+    
+class SQLClothing(Base):
+    __tablename__ = "clothes"
+    
+    id = Column(Integer, primary_key=True)
+    category = Column(Integer)
+    color = Column(Integer)
+    last_used_date = Column(String)
+    image_path = Column(String)
+    
+    @classmethod
+    def from_python(cls, clothing: Clothing) -> SQLClothing:
+        return cls(
+            id=clothing.id,
+            category=clothing.category.name,
+            color=clothing.color.name,
+            last_used_date=str(clothing.last_used_date),
+            image_path=clothing.image_path
+        )
+    
+    def to_python(self) -> Clothing:
+        return Clothing(
+            category=ClothingCategory.from_name(self.category),
+            color=Color.from_name(self.color),
+            last_used_date=datetime.fromisoformat(self.last_used_date),
+            image_path=self.image_path
+        )
 
 
 def create_directory(path: str) -> None:
@@ -14,11 +79,52 @@ def create_directory(path: str) -> None:
 
 
 class DataBase:
-    def __init__(self, *, images_path: str = "images/"):
+    def __init__(self, *, name: str = "wearit.db", images_path: str = "images/"):
+        self.name = name
         create_directory(images_path)
         self.images_path = images_path
+
+        self.engine = create_engine("sqlite:///" + name, echo=True)
+        Base.metadata.create_all(self.engine)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+        # TODO: call self.session.close() somewhere
+    
+    def commit(self) -> None:
+        self.session.commit()
+
+    def add_user(self, id: str) -> None:
+        user = SQLUser(id=id, clothes="[]")
+        self.session.add(user)
+        self.commit()
+    
+    def user_id_exists(self, id: str) -> bool:
+        result = self.session.query(SQLUser).filter_by(id=id).first()
+        return result is not None
+    
+    def ensure_user_exists(self, id: str) -> None:
+        if not self.user_id_exists(id):
+            self.add_user(id)
+    
+    def get_user(self, id: str) -> SQLUser:
+        self.ensure_user_exists(id)
+        return self.session.query(SQLUser).filter_by(id=id).first()
+    
+    def add_clothing(self, user_id: str, item: Clothing) -> None:
+        user = self.get_user(user_id)
+        self.session.add(SQLClothing.from_python(item))
+        user.clothes_append(item)
+        self.commit()
+    
+    def get_garderobe(self, user_id: str) -> list[Clothing]:
+        user = self.get_user(user_id)
+        clothe_ids = user.get_clothes()
+        clothes = self.session.query(SQLClothing).filter(SQLClothing.id.in_(clothe_ids)).all()
+        return clothes
     
     def store_image(self, image: cv2.typing.MatLike, id: str) -> None:
         success = cv2.imwrite(os.path.join(self.images_path, id), image)
         if not success:
             print("Failed to save image")
+
+
