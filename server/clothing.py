e52fec9da556d9b3de119b3beb8c7ef534c0f2b4
maschull106 <matthias.schuller@epfl.ch>
2024-11-30 17:17:56 +0100

    basic clothing python class for the server

---
+++ server/clothing.py
@@ -0,0 +1,46 @@
+from enum import Enum
+from datetime import datetime
+import cv2
+
+
+class ClothingCategory(Enum):
+    TOP = 0
+    BOTTOM = 1
+    SHOES = 2
+
+
+class Color(Enum):
+    UNKNOWN = -1
+    RED = 0
+    GREEN = 1
+    BLUE = 2
+
+
+class Clothing:
+    CLOTHES_COUNT = 0
+    
+    @classmethod
+    def generate_id(cls) -> int:
+        cls.CLOTHES_COUNT += 1
+        return cls.CLOTHES_COUNT - 1
+        
+    def __init__(self, category: ClothingCategory, color: Color = Color.UNKNOWN, last_used_date: datetime = None, image_path: str = ""):
+        if last_used_date is None:
+            last_used_date = datetime.now()
+        self.category = category
+        self.color = color
+        self.last_used_date = last_used_date
+        self._image_path = image_path
+        self._id = Clothing.generate_id()
+    
+    def has_image(self) -> bool:
+        return bool(self._image_path)
+    
+    @property
+    def image_path(self) -> str:
+        if not self.has_image():
+            raise RuntimeError("the clothing does not provide an image")
+        return self._image_path
+    
+    def get_image(self) -> cv2.typing.MatLike:
+        return cv2.imread(self.image_path)
